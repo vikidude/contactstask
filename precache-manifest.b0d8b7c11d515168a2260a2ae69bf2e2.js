6107c98ac944e7cc70919c5818fbd11afebf7d23
vikidude <vigneshwaransasurie@gmail.com>
2020-03-28 12:28:16 +0530

    Updates

---
+++ precache-manifest.b0d8b7c11d515168a2260a2ae69bf2e2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fe08002033bc0b5aacc14adb41251bd",
+    "url": "/contactstask/index.html"
+  },
+  {
+    "revision": "3df1d1b18516e9fb1c44",
+    "url": "/contactstask/static/css/main.901b9b6a.chunk.css"
+  },
+  {
+    "revision": "6f2376af96941359f1bf",
+    "url": "/contactstask/static/js/2.52587f89.chunk.js"
+  },
+  {
+    "revision": "3df1d1b18516e9fb1c44",
+    "url": "/contactstask/static/js/main.c024a88a.chunk.js"
+  },
+  {
+    "revision": "781a63c46db23e451ef6",
+    "url": "/contactstask/static/js/runtime-main.5231a04b.js"
+  }
+]);
